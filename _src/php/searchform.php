d66e0d4a4c219c28a46a6dfbe5d112133cff2abe
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2020-03-08 15:46:23 +0100

    fix search icon path

--- _src/php/searchform.php
+++ _src/php/searchform.php
@@ -2,7 +2,7 @@
   <input type="search" id="search-field" placeholder="Rechercher…" value="<?= get_search_query() ?>" name="s" class="hide-clear-cross">
 
   <label for="search-submit" id="search-submit__icon">
-    <img src="<?= get_template_directory_uri(); ?>/assets/matrix/icons/searchMagnifier_icon_black.png" alt="une loupe représentant la recherche. Cliquer dessus pour lancer la recherche" title="Lancer la recherche" srcset="<?= get_template_directory_uri(); ?>/assets/vector/icons/searchMagnifier_icon_blackk.svg"/>
+    <img src="<?= get_source_path('image-matrix-icon', 'searchMagnifier_icon_black.png'); ?>" alt="une loupe représentant la recherche. Cliquer dessus pour lancer la recherche" title="Lancer la recherche" srcset="<?= get_source_path('image-vector-icon', 'searchMagnifier_icon_black.svg'); ?>"/>
   </label>
   <input type="submit" id="search-submit" class="screen-reader-text">
 </form>
